76f5d9062efe5bdcddbdef8ec609b5b3e067c899
egor dyakonov <egordyakonov2000@gmail.com>
2023-12-27 20:59:17 +0900

    a

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router/router.js";
 import "./assets/styles/style.css";
 import axios from "axios";
-import store from './store';
+import store from "./store";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://superpuperuserposer.pythonanywhere.com/";
 createApp(App).use(store).use(router, axios).mount("#app");
